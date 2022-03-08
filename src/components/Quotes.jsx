6af609ba9e07f4aba214a3a5aaeb21ea2d0eba96
Marcos Vinicius <mvsouza7@gmail.com>
2022-03-08 14:43:43 +0200

    docs: project done

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,16 +1,43 @@
 import React from "react";
+import axios from "axios";
 
 const Quotes = () => {
+  const [quote, setQuote] = React.useState([]);
+  const [author, setAuthor] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((res) => {
+        setQuote(res.data.content);
+        setAuthor(res.data.author);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleFetch = async () => {
+    try {
+      await axios.get("https://api.quotable.io/random").then((res) => {
+        setQuote(res.data.content);
+        setAuthor(res.data.author);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container">
       <div className="container-quote">
         <h1>Quote of the Day</h1>
         <blockquote>
-          <span>“</span>Lorem, ipsum dolor sit amet consectetur adipisicing
-          Omnis culpa provident consectetur.<span>”</span>
-          <p className="author"> - Author</p>
-          {/* <hr /> */}
-          <button>New Quote</button>
+          <span>“</span>
+          {quote}
+          <span>”</span>
+          <p className="author"> - {author}</p>
+          <button onClick={handleFetch}>New Quote</button>
         </blockquote>
       </div>
     </div>
